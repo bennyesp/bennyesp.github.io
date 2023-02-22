678729e0b8aaa938cd163d432326aad26d32e81d
bennyesp <85162802+bennyesp@users.noreply.github.com>
2023-02-22 21:55:46 +0700

    Add files via upload

---
+++ scripts/mainfunc.js
@@ -0,0 +1,92 @@
+const gen={
+    head:{
+        met:{
+            list:[{name:'viewport',content:'width=device-width, initial-scale=1.0'},{charset:'UTF-8'}],
+            exec:function(){
+                let i,mlist=gen.head.met.list;
+                mlen=mlist.length
+                for(i=0;i<mlen;i++){
+                    let current=mlist[i],meta=document.createElement('meta');
+                    if(current.content)meta.content=current.content;
+                    if(current.name)meta.name=current.name;
+                    if(current.charset)meta.setAttribute('charset',current.charset);
+                    document.head.appendChild(meta);
+                }
+            }
+        },
+        font:{
+            list:[{font:'public sans',href:'https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap'}],
+            exec:function(){
+                let i,flist=gen.head.font.list,flen=flist.length;
+                for(i=0;i<flen;i++){
+                    let f=flist[i],font=document.createElement('link');
+                    if(f.href.includes('googleapis')){
+                        font.rel='preconnect';
+                        font.href='https://fonts.googleapis.com';
+                        document.head.appendChild(font);
+                        font=document.createElement('link');
+                        font.rel='preconnect';
+                        font.href='https://fonts.gstatic.com';
+                        font.setAttribute('crossorigin','crossorigin');
+                        document.head.appendChild(font);
+                        font=document.createElement('link');
+                    }
+                    font.rel='stylesheet';
+                    font.href=`${f.href}`;
+                    document.head.appendChild(font);
+                }
+            }
+        },
+        style:{
+            list:['main.css'],
+            func:function(st){
+                let style=document.createElement('link');
+                style.rel='stylesheet';
+                style.href=st;
+                document.head.appendChild(style);
+            },
+            exec:function(addr,ext){
+                const styleloc='styles/';let i,stlist=(!addr)?gen.head.style.list:addr,stlen=stlist.length;
+                if(!ext){for(i=0;i<stlen;i++){gen.head.style.func(`${styleloc}${stlist[i]}`);}}else{gen.head.style.func(addr);}
+            }
+        },
+        title:function(t){
+            let h1;try{h1=document.getElementsByTagName('h1')[0]}catch(e){h1=0;}
+            document.title=(/https:\/\/bennyesp\.github\.io\/?/.test(document.URL))?'Home - Benny ESP GitHub Page':(h1!==0)?`${h1.textContent} - Benny ESP GitHub Page`:`${t} - Benny ESP GitHub Page`;}
+    },
+    navlist:{top:[{name:'Halaman utama',link:'/'},{name:'Penjumlahan berurutan',link:'/penjumlahan-berurutan/index.html'}],
+        foot:[{name:'Kebijakan privasi',link:'/privacy-policy'},{name:'Syarat dan ketentuan',link:'/terms-of-use'}]
+    },
+    nav:function(n){
+        let nav=document.createElement('nav'),nlist,nc,nlc;
+        if(n==='footer'){
+            let footr=document.createElement('footer');
+            document.body.appendChild(footr);
+            nlist=this.navlist.foot;
+            nc='footer-nav',nlc='link-footer';
+            footr.appendChild(nav);
+        }else{
+            nlist=this.navlist.top;
+            nc='top-nav',nlc='topnav-link';
+            document.body.insertAdjacentElement('afterbegin',nav);
+        }
+        nav.classList.add(nc);
+        nlen=nlist.length;
+        for(i=0;i<nlen;i++){
+            let nlin=document.createElement('a');
+            nlin.classList.add(nlc);
+            nlin.href=nlist[i].link;
+            nlin.textContent=nlist[i].name;
+            nav.appendChild(nlin);
+        }
+    },
+    postlist:function(){
+        const datum=getjson('data/post.json');
+        console.log(datum);
+    }
+};
+const getjson=async function(u){
+    let res=await fetch(u);
+    const datum=await res.json();
+    return datum;
+}
