361ba35be2aa51b4306a81cb56bb77d3bc42bbd9
bennyesp <85162802+bennyesp@users.noreply.github.com>
2023-02-22 23:00:44 +0700

    Update mainfunc.js

--- scripts/mainfunc.js
+++ scripts/mainfunc.js
@@ -83,8 +83,9 @@ const gen={
     postlist:function(){
         getjson('/data/post.json').then(
             function(dat){let i,pcount=dat.length;
+                let target=document.getElementById('app-list');
                 for(i=0;i<pcount;i++){
-                    let target=document.getElementById('app-list');let post=dat[i],list=document.createElement('li'),plink=document.createElement('a');
+                    let post=dat[i],list=document.createElement('li'),plink=document.createElement('a');
                     plink.textContent=post.name;
                     plink.href=post.link;
                     target.appendChild(list);
