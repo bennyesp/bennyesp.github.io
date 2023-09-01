303a4d4a787cdd0e1e46535e449732a4d2886224
bennyesp <85162802+bennyesp@users.noreply.github.com>
2023-09-01 11:14:29 +0700

    Create htmlhighlighter.js

---
+++ scripts/htmlhighlighter.js
@@ -0,0 +1,7 @@
+function htmlhighlighter(){
+  let code=document.getElementsByTagName('code');
+  for(a of code){
+    let b=a.textContent;
+    b=b.replace(/(\<)(iframe)/gi)/,'$1<span class="htmltag">$2</span>');
+  }
+}
