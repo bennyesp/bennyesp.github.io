56c31af5ebfa5a7db6e7af382a59a210f6e9cf17
bennyesp <85162802+bennyesp@users.noreply.github.com>
2023-09-01 11:22:03 +0700

    Update htmlhighlighter.js

--- scripts/htmlhighlighter.js
+++ scripts/htmlhighlighter.js
@@ -2,6 +2,6 @@ function htmlhighlighter(){
   let code=document.getElementsByTagName('code');
   for(a of code){
     let b=a.textContent;
-    b=b.replace(/(\<)(iframe)/gi)/,'$1<span class="htmltag">$2</span>');
+    b=b.replace(/(\<)(iframe)/gi),'$1<span class="htmltag">$2</span>');
   }
 }
